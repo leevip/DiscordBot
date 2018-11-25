7aaea6e8c587cfa7999a7aae7f8fbe4a0bd9f9bf
leevip <30495026+leevip@users.noreply.github.com>
2018-11-25 22:58:38 +0200

    Add files via upload

---
+++ discordbot_boris.py
@@ -0,0 +1,24 @@
+#Made by Leevi Pussinen
+from discord.ext.commands import Bot
+from discord import Server, Game
+botprefix = ('!')
+client = Bot(command_prefix='!')
+
+@client.command(pass_context=True,name='game',description='List the players who are playing a game. If the game has spaces in the name, quote it with "".',
+                aliases=['playing'])
+async def game(ctx,gameseek):
+    server = ctx.message.server
+    gamerlist = []
+    for member in server.members:
+        if str(member.game) == gameseek:
+            gamerlist.append(member)
+    gamerlist.append('---------')
+    await client.say('Now playing '+ gameseek)
+    for i in gamerlist:
+        await client.say(i)
+
+@client.event
+async def on_ready():
+    print('Online')
+
+client.run('NTE2MjM5NTc4MzQ5NDM2OTQx.DtwxrQ.VsVjJVTb3--moqp0c8S5u8mr3NE')
